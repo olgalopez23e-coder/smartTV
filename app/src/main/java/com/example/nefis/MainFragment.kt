b54e5970e28657c6047fb75ddec0b35b6df1f3ff
Olga <olga.lopez.23e@utzmg.edu.mx>
2025-12-03 22:23:06 -0600

    Corregido

--- app/src/main/java/com/example/nefis/MainFragment.kt
+++ app/src/main/java/com/example/nefis/MainFragment.kt
@@ -33,11 +33,19 @@ class MainFragment: BrowseSupportFragment() {
         val header1 = HeaderItem(1, title1)
         categories.add(ListRow(header1, videos1))
 
-        // --- Segunda Categoría:
-        val title2 = "Series"
+// --- Segunda Categoría:
+        val title2 = "Caricaturas"
         val videos2 = ArrayObjectAdapter(Card())
         videos2.addAll(0, listOf(
-            Video("cancion", "miraculous", R.mipmap.series, "serie de televisión animada francesa, Miraculous: Las aventuras de Ladybug, y a las joyas mágicas llamadas Miraculous que utilizan los protagonistas de la serie para transformarse en superhéroes", R.raw.mira),
+            Video("caricaturas", "miraculous", R.mipmap.series, "serie de televisión animada francesa, Miraculous: Las aventuras de Ladybug, y a las joyas mágicas llamadas Miraculous que utilizan los protagonistas de la serie para transformarse en superhéroes", R.raw.serie),
+            Video("caricaturas", "Chilly willy", R.mipmap.series, "serie de televisión animada, un pingüino caricaturesco que busca calor y comida, a menudo metiéndose en problemas con un perro llamado Smedley", R.raw.serie1),
+            Video("caricaturas", "Los padrinos magicos", R.mipmap.series, "serie de televisión animada, Timmy Turner, un niño de 10 años a quien sus padres le dejan a cargo de la malvada niñera Vicky. Para ayudarlo, le asignan dos padrinos mágicos: Cosmo y Wanda, quienes le conceden deseos.", R.raw.serie2),
+            Video("caricaturas", "El chavo del 8", R.mipmap.series, "serie de televisión animada, trata de un grupo de personajes que habitan una vecindad, con el protagonista, el Chavo, un niño huérfano que vive en un barril y se mete en líos con los demás por malentendidos o travesuras", R.raw.serie3),
+            Video("caricaturas", "El parajaro loco", R.mipmap.series, "serie de televisión animada, El Pájaro Loco trata principalmente de la película de 2017 donde un abogado, Lance Walters, planea construir una casa en el bosque y entra en conflicto con el Pájaro Loco, un pájaro carpintero que defiende su hogar.", R.raw.serie4),
+
+
+
+
         ))
         val header2 = HeaderItem(2, title2)
         categories.add(ListRow(header2, videos2))
@@ -45,7 +53,7 @@ class MainFragment: BrowseSupportFragment() {
                 // Tercera Categoría
         val title3  = "Musica"
         val videos3 = ArrayObjectAdapter(Card())
-        videos2.addAll(0, listOf(
+        videos3.addAll(0, listOf(
             Video("Canción", "mis gustos", R.mipmap.musica,"Fragmento de cancion que me gusto por alguna razon",R.raw.musica1),
             Video("Canción", "mis gustos", R.mipmap.musica,"Fragmento de cancion que me gusto por alguna razon",R.raw.musica2),
             Video("Canción", "mis gustos", R.mipmap.musica,"Fragmento de cancion que me gusto por alguna razon",R.raw.musica3),
@@ -55,7 +63,7 @@ class MainFragment: BrowseSupportFragment() {
 
 
         ))
-        val header3 = HeaderItem(2, title3)
+        val header3 = HeaderItem(3, title3)
         categories.add(ListRow(header3, videos3))
 
                         // --- Cuarta Categoría
@@ -72,16 +80,20 @@ class MainFragment: BrowseSupportFragment() {
                         ))
                         val header4 = HeaderItem(4, title4)
                         categories.add(ListRow(header4, videos4))
-        /*
-                                // --- 5. Quinta Categoría: Delfín ---
-                                val title5 = "Delfín"
+
+                                // Quinta Categoría
+                                val title5 = "Videos entetenidos pero raros"
                                 val videos5 = ArrayObjectAdapter(Card())
                                 videos5.addAll(0, listOf(
-                                    Video("saltos", "acrobacia", R.mipmap.imagen_g, "Delfines jugando en la bahía.", R.raw.video_g),
-                                    Video("inteligencia", "ciencia", R.mipmap.imagen_h, "Comunicación y sonar del Delfín.", R.raw.video_h)
-                                ))
+                                    Video("youtube", "", R.mipmap.videos, "introduccion de 25 cosas que no sabias", R.raw.videos),
+                                    Video("youtube", "viaje", R.mipmap.videos, "video de exploracion en alguna tierra de españa", R.raw.videos1),
+                                    Video("youtube", "viaje", R.mipmap.videos, "24 horas en una casa del arbol", R.raw.videos2),
+                                    Video("youtube", "chef", R.mipmap.videos, "siendo chef por 24hr.", R.raw.videos3),
+                                    Video("youtube", "raro", R.mipmap.videos, "100 españoles dijeron", R.raw.videos4),
+
+                                   ))
                                 val header5 = HeaderItem(5, title5)
-                                categories.add(ListRow(header5, videos5))*/
+                                categories.add(ListRow(header5, videos5))
 
         // 2. Asignar el adaptador al fragmento
         adapter = categories
